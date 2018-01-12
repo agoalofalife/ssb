18d74156207666aa8f140ef0b169ed0c179beaaa
agoalofalife <agoalofalife@gmail.com>
2018-01-12 12:37:17 +0300

    insulated in cli server command

--- bin/app.js
+++ bin/app.js
@@ -7,35 +7,37 @@ const program = require('commander');
 const { version } = require('../helper');
 const {routes} = require('../route/routes');
 
-// const Server = require('../Server/Server').app;
+const Server = require('../Server/Server');
 const urlAuth = 'https://slack.com/api/oauth.access';
 const axios = require('axios');
 // TODO Create cli application for  the entire process of obtaining a token
 
 program
     .version(version)
-    .description('Console application')
-    // .command('server')
-    // .option('-s, --start', 'Start server in waiting for redirect url')
-    // .action(function (dir, cmd) {
-    //     // TODO  check on parameter -s
-    //     Server.get('/', (req, res) => {
-    //         axios.get(urlAuth, {
-    //             params:{
-    //                 client_id: process.env.SLACK_CLIENT_ID,
-    //                 client_secret: process.env.SLACK_CLIENT_SECRET,
-    //                 code: req.query.code,
-    //             }
-    //         })
-    //         .then(function (response) {
-    //             console.log(response.data, 'authorize');
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    //     });
-    // })
-    .command('show <what>')
+    .description('Work with server')
+    .command('server')
+    .option('-s, --start', 'Start server in waiting for redirect url')
+    .action(function (options) {
+        if (options.start) {
+            (new Server()).app.get('/', (req, res) => {
+                axios.get(urlAuth, {
+                    params:{
+                        client_id: process.env.SLACK_CLIENT_ID,
+                        client_secret: process.env.SLACK_CLIENT_SECRET,
+                        code: req.query.code,
+                    }
+                })
+                .then(function (response) {
+                    console.log(response.data, 'authorize');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            });
+        }
+    });
+
+    program.command('show <what>')
     .description('Show information')
     .action(function (what, options) {
         switch (what) {

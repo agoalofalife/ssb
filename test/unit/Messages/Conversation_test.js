663eb776b96ed0355f9a7900a86a668dd827292d
agoalofalife <agoalofalife@gmail.com>
2018-01-13 11:20:22 +0300

    add tests conversation

---
+++ test/unit/Messages/Conversation_test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const Conversation = require('./../../../Messages/Conversation');
+const faker = require('faker');
+
+let SlackBotFake = {};
+let fakeResponse = {
+    callback_id:faker.random.uuid()
+};
+
+let conversationObject = new Conversation(fakeResponse, SlackBotFake);
+
+describe('Conversation', function() {
+    describe('#typeEvent', function() {
+        it('exist property typeEvent and return string type', function() {
+            assert.equal(conversationObject.typeEvent, 'conversation');
+        });
+    });
+    describe('#descriptionEvent', function() {
+        it('exist property typeEvent and return string type', function() {
+            assert.equal(conversationObject.descriptionEvent, 'The event occurs when it was sent interactive message');
+        });
+    });
+
+    describe('#compareResponse', function() {
+        it('get text from response callback_id', function() {
+            assert.equal(conversationObject.compareResponse, fakeResponse.callback_id);
+        });
+    });
+});

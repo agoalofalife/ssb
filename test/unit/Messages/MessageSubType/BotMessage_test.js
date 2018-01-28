53a6eb0403da67857669d462b056d7eb45fecea2
agoalofalife <agoalofalife@gmail.com>
2018-01-28 22:50:49 +0300

    fix tests bot message subtype

--- test/unit/Messages/MessageSubType/BotMessage_test.js
+++ test/unit/Messages/MessageSubType/BotMessage_test.js
@@ -1,17 +1,26 @@
 const BotMessage = require('./../../../../Messages/MessageSubType/BotMessage');
 const DirectMessage = require('./../../../../Messages/MessageBase/DirectMessage');
 const assert = require('assert');
+const faker = require('faker');
+let  fakeResponseSlack = {
+    subtype:'bot_message',
+    bot_id:faker.random.uuid(),
+    text:faker.random.word()
+};
 
-// class BaseMessage extends Message{
-//
-// }
 describe('BotMessage', () => {
-    it('expected property name event', function () {
+    it('expected property name event', () => {
         // todo I think these costs add more options
             let directMessage = new DirectMessage();
             assert.equal((new BotMessage('', '', directMessage)).typeEvent, `${directMessage.typeEvent}.bot_message`);
     });
-    it('expected valid description', function () {
+    it('expected valid description', () => {
         assert.equal((new BotMessage()).descriptionEvent, 'The event occurs when a message was sent by a bot in the channel');
+    });
+    it('route test method is true', () => {
+        assert.equal(BotMessage.route(fakeResponseSlack), true)
+    });
+    it('check property responseSlack text', () => {
+        assert.equal((new BotMessage(fakeResponseSlack)).compareResponse, fakeResponseSlack.text)
     });
 });
